65dcb735ea7e7e6ecbc0805d795db0849e47fa3d
sirkrawell <eduardolias07@gmail.com>
2025-12-22 17:19:36 -0400

    nuevo commit

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ function crearElementoDiv(arregloProyectos) {
 
 
 agregarAlArreglo("img/adivina-la-bandera.jpeg","Adivina La Bandera",["html","css","javascript"],"medio",true,"https://sirkrawell.github.io/adivina-la-bandera/")
-agregarAlArreglo("img/adivina-la-palabra.png","Adivina la palabra",["html","css"],"medio",true,"https://sirkrawell.github.io/adivinar-palabra/")
+agregarAlArreglo("img/adivina-la-palabra.png","Adivina la palabra",["html","css","javascript"],"medio",true,"https://sirkrawell.github.io/adivinar-palabra/")
 
 
 
